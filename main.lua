62e07b28852f7ae9f1c76740af96b0dd2dbe12d8
HellFire <vaxure1337@gmail.com>
2020-08-28 19:19:52 -0230

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,11 @@
+local ReplicatedStorage = game.ReplicatedStorage;
+local Name = game.Players.LocalPlayer.Name;
+local SwordSwing = game:GetService("Workspace")[Name]["Black Sword"].Handle.SwordSwing;
+
+if ReplicatedStorage:WaitForChild("Setting"):InvokeServer("Get", "SFX") == true then
+	SwordSwing:Play();
+end
+
+while wait() do
+    ReplicatedStorage:WaitForChild("Swing"):FireServer(1);
+end
